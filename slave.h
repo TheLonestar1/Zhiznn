6862db731d7c18cf97e92892cf5bc9376f50ddbe
Sergey Tokarev <ssodq093@gmail.com>
2019-05-03 16:22:34 +0700

    Только наброски алг. поиска веток

---
+++ slave.h
@@ -0,0 +1,15 @@
+void slave(int *pozition) /*передаём адресс расположения муровья*/ {
+	//поиск типа "змейка"
+	int r = 1;//радиус поиска
+	int limit = r * 2 + 1;//ограничение для поиска по "оси"
+	int limit_sum = limit^2 - 1;//ограничение для поиска (общее)
+	int count_step = (limit * 2) - 1);//шаг счётчика
+	int stick = 0;//ветка найдена? (0/1)
+	
+	for (int i = 0; i < limit_sum; i + count_step) {
+		for (int i = 0; i < limit; i++) {
+			if () {
+			}
+		}
+	}
+}
